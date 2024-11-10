359831aa9235c484da5b6460a3d9515c28a678d6
dejancejic <dejandejan5207@gmail.com>
2024-11-10 19:28:34 +0100

    Osnovna stranica GUI i info stranica

--- MemoryMaster/MemoryMaster/MainWindow.xaml.cs
+++ MemoryMaster/MemoryMaster/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using MemoryMaster.Pages;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,54 @@ namespace MemoryMaster
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+
+
+        private void languageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender != null)
+            {
+                SetLanguage(((Button)sender).Tag.ToString());
+
+            }
+        }
+
+            private void SetLanguage(string language)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(language);
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(language);
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            ResourceDictionary resdict = new ResourceDictionary()
+            { 
+            Source=new Uri($"Resources/Dictionary-{language}.xaml",UriKind.Relative)
+            };
+
+            Application.Current.Resources.MergedDictionaries.Add(resdict);
+
+            enBtn.IsEnabled = true;
+            srBtn.IsEnabled = true;
+
+            switch (language) {
+                case "en": enBtn.IsEnabled = false;
+                    break;
+                case "sr":srBtn.IsEnabled = false; 
+                    break;
+            }
+
+        }
+
+        private void infoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AppInfoPage secondWindow = new AppInfoPage(); 
+            secondWindow.Closed += SecondWindow_Closed; 
+            secondWindow.Show(); this.Hide();
+            this.Hide();
+        }
+        private void SecondWindow_Closed(object sender, System.EventArgs e) { 
+            this.Show();
         }
     }
 }
